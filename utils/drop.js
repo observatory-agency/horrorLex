dfec6aef64b1fb1e352327b6301d62e05a3d187c
John Foderaro <johnfoderaro@icloud.com>
2020-10-29 20:34:10 -0400

    feat: proof of concept for csv to mongo #2

--- utils/drop.js
+++ utils/drop.js
@@ -1,1 +1,27 @@
-// scripts for dropping test collections and databases
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const {
+  DB_CONNECTION,
+  DB_NAME,
+  DB_PORT,
+} = process.env;
+
+const mongoClient = new MongoClient(`${DB_CONNECTION}:${DB_PORT}`, { useUnifiedTopology: true });
+
+mongoClient.connect(async (connectionError, client) => {
+  if (connectionError) {
+    return console.error(connectionError);
+  }
+  try {
+    const db = client.db(DB_NAME);
+    const books = db.collection('books') || await db.createCollection('books');
+    const success = await books.drop();
+    if (success) {
+      console.log('Dropped "books" collection!');
+    }
+    mongoClient.close();
+  } catch (error) {
+    return console.error(error);
+  }
+});
