3b65a4d7a5b3814a33eeeb42be53f040f3ad71a3
John Foderaro <johnfoderaro@icloud.com>
2020-11-04 23:06:17 -0500

    chore: progress commit #20

--- controllers/books.js
+++ controllers/books.js
@@ -1,10 +1,79 @@
+// TODO - this may be controlled in the UI
+const DOCS_PER_PAGE = 10;
+
 async function getAll(req, res, next) {
+  // try {
+  //   const { app: { locals: { books } } } = req;
+  //   const allDocuments = await books.find().toArray();
+  //   // const document = documents[Math.floor(Math.random() * documents.length - 1)];
+  //   const documents = allDocuments.slice(0, 10);
+  //   return documents ? res.render('results', { documents }) : res.sendStatus(500);
+  // } catch (error) {
+  //   return next(error);
+  // }
   try {
-    const { app: { locals: { books } } } = req;
-    const allDocuments = await books.find().toArray();
-    // const document = documents[Math.floor(Math.random() * documents.length - 1)];
-    const documents = allDocuments.slice(0, 10);
-    return documents ? res.render('results', { documents }) : res.sendStatus(500);
+    const {
+      app: { locals: { books } },
+      query: {
+        count,
+        page,
+        search,
+        sort,
+        tag,
+      },
+    } = req;
+
+    const pageNumber = parseInt(page, 10) || 1;
+    const skip = (pageNumber - 1) * DOCS_PER_PAGE;
+
+    const query = [];
+    const text = { $match: { $text: { $search: search } } };
+    const match = { $match: { tags: tag } };
+    const facet = {
+      $facet: {
+        // TODO add sort
+        documents: [{ $skip: skip }, {
+          $limit: parseInt(count, 10) || 10,
+        }],
+        total: [{ $count: 'count' }],
+      },
+    };
+
+    if (tag) {
+      query.push(match);
+    }
+
+    if (search) {
+      query.push(text);
+    }
+
+    query.push(facet);
+
+    const [results] = await books.aggregate(query).toArray();
+    const { documents } = results;
+
+    if (!documents.length > 0) {
+      return res.render('results', {
+        noResults: true,
+        param: search,
+      });
+    }
+
+    const total = results.total[0].count;
+    const range = (documents.length + skip);
+    const nextPage = (range < total) ? (pageNumber + 1) : null;
+    const lastPage = (pageNumber !== 1) ? (pageNumber - 1) : null;
+
+    return res.render('results', {
+      documents,
+      count,
+      lastPage,
+      nextPage,
+      range,
+      total,
+      param: tag || search,
+      type: (tag && 'tag') || (search && 'search'),
+    });
   } catch (error) {
     return next(error);
   }
