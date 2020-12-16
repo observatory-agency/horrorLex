045a5aa0e1a8c533dec563d78ef34b2efc0340b5
John Foderaro <john@maelstrom.local>
2020-12-15 22:36:02 -0500

    chore: browse page proof of concept #24

--- src/scripts/Books.js
+++ src/scripts/Books.js
@@ -59,9 +59,8 @@ class Books {
   }
 
   static quickSearchHandler(event) {
-    const enter = 'Enter';
     const { dataset, value } = event.target;
-    const enterKeyDown = event.key === enter;
+    const enterKeyDown = event.key === Books.enterKey;
     if (enterKeyDown && dataset.handler && dataset.handler === Books.quickSearch) {
       window.location.href = Books.getResultsUtil({
         count: 10,
@@ -102,6 +101,7 @@ class Books {
 Books.browse = 'browse';
 Books.browseEndPoint = '/browse';
 Books.browseSelector = 'div[data-container="browse"]';
+Books.enterKey = 'Enter';
 Books.paramPage = 'page';
 Books.paramSearch = 'search';
 Books.paramTag = 'tag';
