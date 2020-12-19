eb16a9ae87f583c75fc6aed8100c2f9f4350505d
John Foderaro <john@maelstrom.local>
2020-12-18 23:03:15 -0500

    chore: fix up project pipeline for production

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: './src/scripts/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/assets'),
     filename: 'app.js',
     publicPath: '/public',
   },
