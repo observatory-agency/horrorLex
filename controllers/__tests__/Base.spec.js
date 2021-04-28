b937824a53d12640506ff9046c438a3dc771e809
John Foderaro <john@maelstrom.local>
2021-04-27 22:58:35 -0400

    chore: remove commented code

--- controllers/__tests__/Base.spec.js
+++ controllers/__tests__/Base.spec.js
@@ -2,27 +2,10 @@ const BaseController = require('../Base');
 
 jest.mock('../../models/Book');
 
-// class SomeOtherController extends BaseController {
-//   constructor() {
-//     super();
-//     this.get = 'get';
-//     this.post = 'post';
-//   }
-
-//   get() {
-//     return this.get;
-//   }
-
-//   post() {
-//     return this.post;
-//   }
-// }
-
 describe('BaseController', () => {
   let base;
   beforeEach(() => {
     base = new BaseController();
-    // someOtherController = new SomeOtherController();
   });
 
   describe('constructor', () => {
