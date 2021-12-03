96d7685d19e4e3d4c59ed4cf4b109e2526269a74
John Foderaro <john@maelstrom.local>
2021-12-03 09:01:30 -0500

    chore: progress commit

--- controllers/StoryBlok.js
+++ controllers/StoryBlok.js
@@ -1,17 +1,16 @@
 const BaseController = require('./Base');
+const BlogEngine = require('../lib/BlogEngine');
 
 class StoryBlokController extends BaseController {
-  constructor() {
-    super();
-  }
-
   post(req, res, next) {
     const { headers, body } = req;
+    const blog = new BlogEngine();
+    const signature = headers['webhook-signature'];
     try {
-      const signature = headers['webhook-signature'];
-      const verifiedSignature = this.verifySignature(body, signature);
-      return verifiedSignature ? res.sendStatus(201) : res.sendStatus(422);
-    } catch(error) {
+      return this.verifySignature(body, signature)
+        ? res.sendStatus(201) && blog.build(body)
+        : res.sendStatus(422);
+    } catch (error) {
       return next(error);
     }
   }
